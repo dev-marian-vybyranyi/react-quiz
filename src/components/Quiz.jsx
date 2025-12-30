93579cf07edca5acce532edfac1332d836bd67a2
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-30 14:18:42 +0200

    Add QuestionTimer component and integrate it into Quiz to display a question countdown.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizComplete from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -28,6 +29,7 @@ const Quiz = () => {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(ull)} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
